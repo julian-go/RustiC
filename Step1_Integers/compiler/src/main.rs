8cb78d6a14077614db8b37185b6393e9821acb8a
Julian Gottwald <mail@juliangottwald.com>
2020-06-23 08:28:48 +0200

    Added tasks file and removed unneeded code and files

--- Step1_Integers/compiler/src/main.rs
+++ Step1_Integers/compiler/src/main.rs
@@ -4,15 +4,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    // if args.len() > 1 {
     let tokens: Vec<String> = match lexer::tokenize(args[1].clone()) {
         Ok(tokens) => tokens,
         Err(err) => panic!("{:?}", err)
     };
-//         println!("Hello, {:?}!", tokens);
-//     } else {
-//         println!("No .c file given for compilation");
-//         return;
-//     }
+    println!("Hello, {:?}!", tokens);
+
 }
